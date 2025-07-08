4678922ab81efcca6ac294765364e565f53c75a2
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-07-08 22:03:54 +0530

    Revert "Initialize allowedOrigins list if null in getter"
    This reverts commit eac5bb07a052e9c9a76ec23e21bc243146fe6db8.

--- components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/application/management/v1/OpenIDConnectConfiguration.java
+++ components/org.wso2.carbon.identity.api.server.application.management/org.wso2.carbon.identity.api.server.application.management.v1/src/gen/java/org/wso2/carbon/identity/api/server/application/management/v1/OpenIDConnectConfiguration.java
@@ -217,12 +217,8 @@ public enum StateEnum {
     @JsonProperty("allowedOrigins")
     @Valid
     public List<String> getAllowedOrigins() {
-        if (this.allowedOrigins == null) {
-            this.allowedOrigins = new ArrayList<>();
-        }
         return allowedOrigins;
     }
-
     public void setAllowedOrigins(List<String> allowedOrigins) {
         this.allowedOrigins = allowedOrigins;
     }
